bcf9a220337afe156f45a06f0e301762f8ec056d
Natalie L <natalie@systemnatalie.com>
2025-04-07 14:52:42 -0500

    Added California bill scraping functionality

---
+++ California.py
@@ -0,0 +1,30 @@
+from dataclasses import fields
+
+import urllib3
+from urllib.parse import quote_plus
+import requests
+import os
+from bs4 import BeautifulSoup
+import bs4.element
+http = urllib3.PoolManager()
+
+response=http.request("GET","https://leginfo.legislature.ca.gov/faces/billSearchClient.xhtml")
+soup= BeautifulSoup(response.data, 'html.parser')
+session_years = soup.find("select",{"id":"session_year"}).find("option").attrs["value"]
+print("got session")
+
+bills_url=f'https://leginfo.legislature.ca.gov/faces/billSearchClient.xhtml?session_year={session_years}'
+response=http.request("GET",bills_url)
+soup = BeautifulSoup(response.data, 'html.parser')
+bills_table=soup.find("table",{"id":"bill_results"}).find("tbody")
+bill_final_url="https://leginfo.legislature.ca.gov"
+ct=0
+for row in bills_table.find_all("tr"):
+	link=row.find("a")
+	bill_data_response = http.request("GET", bill_final_url+link.attrs["href"])
+	bill_soup = BeautifulSoup(bill_data_response.data, 'html.parser')
+	bill_dat=bill_soup.find("div",{"id":"bill_all"})
+	with open("bills/california/"+link.text.strip()+".html",'w+', encoding='utf-8') as f:
+		f.write(bill_dat.prettify())
+	print("got bill: "+str(ct))
+	ct+=1
